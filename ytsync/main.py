a85634125513aa6b37e0264151c60289f7e6dfdc
Vignesh Rao <svignesh1793@gmail.com>
2026-08-23 22:07:33 -0500

    perf: Simplify logging startup configuration

--- ytsync/main.py
+++ ytsync/main.py
@@ -92,32 +92,13 @@ def webhook_is_usable() -> bool:
 def log_config() -> None:
     """Log all safe env configuration."""
     LOGGER.debug("***************************** CONFIGURATION START *****************************")
-    LOGGER.debug("Host: %s", config.env.host)
-    LOGGER.debug("Port: %s", config.env.port)
-    LOGGER.debug("Timezone: %s", config.env.tz)
-    LOGGER.debug("Max retries: %d", config.env.max_retries)
-    LOGGER.debug("Backoff factor: %d", config.env.backoff_factor)
-    LOGGER.debug("Max transfers: %d", config.env.max_transfers)
-    LOGGER.debug("Delayed start: %s", config.env.delayed_start)
-    LOGGER.debug("Next buffer: %d", config.env.next_buffer)
-    LOGGER.debug("Cooldown interval: %d", config.env.cooldown_interval)
-    LOGGER.debug("Max error threshold: %d", config.env.max_error_threshold)
-    LOGGER.debug("Data directory: %s", config.env.data_dir)
-    LOGGER.debug("Logs directory: %s", config.env.logs_dir)
-    LOGGER.debug("Download directory: %s", config.env.download_dir)
-    LOGGER.debug("Database: %s", config.env.database)
-    LOGGER.debug("Allowed bot IDs: %s", config.env.bot_chat_ids)
-    LOGGER.debug("Allowed bot users: %s", config.env.bot_users)
-    LOGGER.debug("Poll interval: %s", config.env.poll_interval)
-    LOGGER.debug("Default tracker: %s", config.env.default_tracker)
-    LOGGER.debug("Remote host: %s", config.env.remote_host)
-    LOGGER.debug("Remote user: %s", config.env.remote_user)
-    LOGGER.debug("Remote path: %s", config.env.remote_path)
-    LOGGER.debug("Delete after sync: %s", config.env.delete_after_sync)
-    LOGGER.debug("Bot webhook: %s", config.env.bot_webhook)
-    LOGGER.debug("Bot webhook IP: %s", config.env.bot_webhook_ip)
-    LOGGER.debug("Bot endpoint: %s", config.env.bot_endpoint)
-    LOGGER.debug("Bot certificate: %s", config.env.bot_certificate)
+    # TODO: Hard-coded; find a better way
+    sensitive = ("log_config", "bot_token", "bot_secret", "apikey")
+    for key, value in config.env.model_dump().items():
+        if key in sensitive:
+            continue
+        key = key.capitalize().replace("_", " ").replace("dir", "directory")
+        LOGGER.debug("%s: %s", key, value)
     LOGGER.debug("***************************** CONFIGURATION END *****************************")
 
 
